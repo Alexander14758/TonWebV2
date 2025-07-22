eea488f03fdf0bef190b96133c4fc24cbb05415b
EarnOnChain <copaywallet000000000000@gmail.com>
2025-07-22 15:44:21 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,6 +1,6 @@
 
     // Set the airdrop end date
-    const endDate = new Date("2025-07-22T20:59:59").getTime(); // Change this date
+    const endDate = new Date("2025-07-23T19:59:59").getTime(); // Change this date
 
     function updateTimer() {
       const now = new Date().getTime();
